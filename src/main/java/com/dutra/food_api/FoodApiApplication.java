5dea7cb8dd1abd7070ac5553d9b01af9e519f82b
David Alves Dutra <dadutra@hotmail.com>
2025-02-17 14:44:02 -0300

    Entidade Cozinha.

--- src/main/java/com/dutra/food_api/FoodApiApplication.java
+++ src/main/java/com/dutra/food_api/FoodApiApplication.java
@@ -1,5 +1,6 @@
 package com.dutra.food_api;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FoodApiApplication.class, args);
+
 	}
 
 }
