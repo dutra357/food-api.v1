e6f1f1cf2bbe86a9fba7b5a8a6d4cb238cb839e6
David Alves Dutra <dadutra@hotmail.com>
2025-02-18 08:40:30 -0300

    Classe Permissao. + seed.

---
+++ src/main/java/com/dutra/food_api/entities/Permissao.java
@@ -0,0 +1,62 @@
+package com.dutra.food_api.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_permissao")
+public class Permissao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String descricao;
+
+    public Permissao() {}
+    public Permissao(Long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Permissao permissao = (Permissao) o;
+        return Objects.equals(id, permissao.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
